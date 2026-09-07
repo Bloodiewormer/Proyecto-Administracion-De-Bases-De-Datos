0a27a7ba9bf7aa9f8302ded0832f198777f0078d
Alice Fajardo <147007299+alicefajardoo@users.noreply.github.com>
2026-09-07 17:50:48 -0600

    Configuración inicial del proyecto con Spring Boot y Java 21

---
+++ src/test/java/cr/ac/una/dbmonitor/DbMonitorApplicationTests.java
@@ -0,0 +1,12 @@
+package cr.ac.una.dbmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
